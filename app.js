60cff454277b5a2ccd7d590b098059b6ea1e7457
Jean-PierrePapin <skouo82@gmail.com>
2020-05-26 19:04:41 +0100

    Updating the budget: Budget Controller

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ var budgetController = (function() {
         this.value = value;
     };
 
+    var calculateTotal = function( type ) {
+        var sum = 0;
+        data.allItems[type].forEach( function ( cur ) {
+            sum += cur.value;
+        } );
+        data.totals[type] = sum;
+    };
+
     var data = {
         allItems: {
             exp: [],
@@ -21,7 +29,9 @@ var budgetController = (function() {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     };
 
     return {
@@ -48,8 +58,41 @@ var budgetController = (function() {
             data.allItems[type].push( newItem );
             // Return the new element
             return newItem;
+        },
+
+        calculateBudget: function() {
+
+            // Calculate total income and expenses
+            calculateTotal( 'exp' );
+            calculateTotal( 'inc' );
+
+            // Calculate the budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+
+            // Calculate the percentage of income that we spent
+            if( data.totals.inc > 0 ) {
+                data.percentage = Math.round( (data.totals.exp/data.totals.inc) * 100 );
+            } else {
+                data.percentage = -1;
+            }
+            
+
+        },
+
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            };
+        },
+
+        testing: function() {
+            console.log( data );
         }
-    }
+
+    };
 
 })();
 
@@ -70,7 +113,7 @@ var UIController = (function() {
             return {
                 type: document.querySelector( DOMstrings.inputType ).value, // Will be either inc or exp
                 description: document.querySelector( DOMstrings.inputDescription ).value,
-                value: document.querySelector( DOMstrings.inputValue ).value
+                value: parseFloat( document.querySelector( DOMstrings.inputValue ).value )
             };            
         },
 
@@ -102,7 +145,7 @@ var UIController = (function() {
         clearFields: function() {
             var fields, fieldsArr;
 
-            fields = docment.querySelectorAll( DOMstrings.inputDescription + ', ' + DOMstrings.inputValue );
+            fields = document.querySelectorAll( DOMstrings.inputDescription + ', ' + DOMstrings.inputValue );
 
             fieldsArr = Array.prototype.slice.call( fields );
 
@@ -138,6 +181,15 @@ var controller = (function(budgetCtrl, UICtrl) {
         } );
     }
 
+    var updateBudget = function() {
+        // 1. Calculate the budget
+        budgetCtrl.calculateBudget();
+
+        // 2. Return the budget
+        var budget = budgetCtrl.getBudget();
+
+        // 3. Display the budget on the UI
+    }
     
 
     var ctrlAddItem = function() {
@@ -146,18 +198,23 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 1. Get the filed input data
         input = UICtrl.getInput();
 
-        // 2. Add the item to the budget controller
-        newItem = budgetCtrl.addItem( input.type, input.description, input.value );
+        if( input.description !== "" && !isNaN(input.value) && input.value > 0 ) {
+            
+            // 2. Add the item to the budget controller
+            newItem = budgetCtrl.addItem( input.type, input.description, input.value );
 
-        // 3. Add the item to the UI
-        UICtrl.addListItem( newItem, input.type );
+            // 3. Add the item to the UI
+            UICtrl.addListItem( newItem, input.type );
 
-        // 4. Clear the fields
-        UICtrl.clearFields();
+            // 4. Clear the fields
+            UICtrl.clearFields();
 
-        // 4. Calculate the budget
+            // 5. Calculate and update budget
+            updateBudget();
+        }
 
-        // 5. Display the budget on the UI
+        
+
     };
     
     return {
